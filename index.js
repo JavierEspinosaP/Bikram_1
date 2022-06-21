96ac2c5a1eae6e04f8dcbf2330e38f1436555b37
Javier Espinosa <javierespinosapasamontes@gmail.com>
2022-06-21 09:35:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+
+
+//Ejercicio 11
+
+var postI;
+
+var postJ;
+
+postI = 0;
+
+postJ = 0;
+
+for (let i = 0; i < 11; i++) {
+    postI += postJ++
+}
+
+//Ejercicio 12
+
+ let sumaPares = 0;
+
+ for (let i = 0; i < 10; i++) {
+    if (i%2 == 0) {
+        sumaPares =+ i;
+    }
+ }
+
+//Ejercicio 24
+
+var contarHasta10
+
+contarHasta10 = 0;
+
+while (contarHasta10 !== 10) {
+    contarHasta10++;
+} 
+
+//Ejercicio 26
+
+var sumaImpares
+
+sumaImpares = 0;
+
+for (let i = 0; i < 10; i++) {
+    if (i%2 !== 0) {
+        sumaImpares =+ i;
+    }
+}
+
+
